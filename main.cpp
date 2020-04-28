fa063c3fb922cc11903ca83d8380bc674c5825a6
SunJiee <760941378@qq.com>
2020-04-29 00:05:56 +0800

    no message

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
+#include <vector>
 #include <cmath>
 #include "common/tgaimage.h"
+#include <iostream>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor green = TGAColor(0,   255, 0,   255);
+const int width  = 200;
+const int height = 200;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     bool steep = false;
@@ -34,13 +39,69 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     }
 }
 
-int main(int argc, char** argv) {
-    TGAImage image(100, 100, TGAImage::RGB);
-    for (int i=0; i<1000000; i++) {
-        line(13, 20, 80, 40, image, white);
-        line(20, 13, 40, 80, image, red);
-        line(80, 40, 13, 20, image, red);
+void triangle(int t0[], int t1[], int t2[], TGAImage &image, TGAColor color) {
+    // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!)
+    if(t0[1]>t1[1]) std::swap(t0, t1);
+    if(t0[1]>t2[1]) std::swap(t0, t2);
+    if(t1[1]>t2[1]) std::swap(t1, t2);
+    int total_height = t2[1]-t0[1];
+    // for (int y = t0[1]; y < t1[1]; y++){
+    //     int segment_height = t1[1] - t0[1] + 1;
+    //     float alpha = (float)(y - t0[1]) / total_height;
+    //     float beta = (float)(y - t0[1]) / segment_height; // be careful with divisions by zero
+    //     float A[2] = {t0[0] + (t2[0] - t0[0]) * alpha, t0[1] + (t2[1] - t0[1]) * alpha};
+    //     float B[2] = {t0[0] + (t1[0] - t0[0]) * beta, t0[1] + (t1[1] - t0[1]) * beta};
+    //     if (A[0]>B[0]) std::swap(A, B);
+    //     for (int j=A[0]; j<=B[0]; j++) {
+    //         image.set(j, y, color); // attention, due to int casts t0.y+i != A.y
+    //     }
+    // }
+    // for (int y = t1[1]; y < t2[1]; y++){
+    //     int segment_height = t2[1] - t1[1] + 1;
+    //     float alpha = (float)(y - t0[1]) / total_height;
+    //     float beta = (float)(y - t1[1]) / segment_height; // be careful with divisions by zero
+    //     float A[2] = {t0[0] + (t2[0] - t0[0]) * alpha, t0[1] + (t2[1] - t0[1]) * alpha};
+    //     float B[2] = {t1[0] + (t2[0] - t1[0]) * beta, t1[1] + (t2[1] - t1[1]) * beta};
+    //     if (A[0]>B[0]) std::swap(A, B);
+    //     for (int j=A[0]; j<=B[0]; j++) {
+    //         image.set(j, y, color); // attention, due to int casts t0.y+i != A.y
+    //     }
+    // }
+    for (int y = t0[1]; y < t1[1]; y++){
+        int segment_height = t1[1] - t0[1] + 1;
+        float alpha = (float)(y - t0[1]) / total_height;
+        float beta = (float)(y - t0[1]) / segment_height; // be careful with divisions by zero
+        int A = t0[0] + (t2[0] - t0[0]) * alpha;
+        int B = t0[0] + (t1[0] - t0[0]) * beta;
+        if (A>B) std::swap(A, B);
+        for (int j=A; j<=B; j++) {
+            image.set(j, y, color); // attention, due to int casts t0.y+i != A.y
+        }
     }
+    for (int y = t1[1]; y < t2[1]; y++){
+        int segment_height = t2[1] - t1[1] + 1;
+        float alpha = (float)(y - t0[1]) / total_height;
+        float beta = (float)(y - t1[1]) / segment_height; // be careful with divisions by zero
+        int A = t0[0] + (t2[0] - t0[0]) * alpha;
+        int B = t1[0] + (t2[0] - t1[0]) * beta;
+        if (A>B) std::swap(A, B);
+        for (int j=A; j<=B; j++) {
+            image.set(j, y, color); // attention, due to int casts t0.y+i != A.y
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    TGAImage image(width, height, TGAImage::RGB);
+
+    int t0[3][2] = {{10, 70},   {50, 160},  {70, 80}};
+    int t1[3][2] = {{180, 50},  {150, 1},   {70, 180}};
+    int t2[3][2] = {{180, 150}, {120, 160}, {130, 180}};
+
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
+
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     return 0;

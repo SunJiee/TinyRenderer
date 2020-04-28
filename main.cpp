32427472d56a0576fd4be51bc2c1c5028e244d76
SunJiee <760941378@qq.com>
2020-04-28 21:42:28 +0800

    no message

--- main.cpp
+++ main.cpp
@@ -22,18 +22,18 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     int y = y0;
     for (int x=x0; x<=x1; x++) {
         if (steep) {
-            image.set(y, x, TGAColor(255, 1));
+            image.set(y, x, color);
         } else {
-            image.set(x, y, TGAColor(255, 1));
+            image.set(x, y, color);
         }
         error2 += derror2;
-
-        if (error2>dx) {
+        if (error2 > dx) {
             y += (y1>y0?1:-1);
             error2 -= dx*2;
         }
     }
 }
+
 int main(int argc, char** argv) {
     TGAImage image(100, 100, TGAImage::RGB);
     for (int i=0; i<1000000; i++) {
